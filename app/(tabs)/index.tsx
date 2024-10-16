352d6d4707f4e202448faa8c97297d7bbcf01173
Lance <lance@lancesnider.com>
2024-10-16 13:33:45 -0700

    Get old rive NOT working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,50 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { StyleSheet } from 'react-native'
 
 import EditScreenInfo from '@/components/EditScreenInfo'
 import { Text, View } from '@/components/Themed'
-// import Rive, { Alignment, Fit, RiveRef } from 'rive-react-native'
+import firebase from '@react-native-firebase/app'
+
+import Rive, { Alignment, Fit, RiveRef } from 'rive-react-native'
+
+const credentials = {
+  clientId: '',
+  appId: '',
+  apiKey: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  projectId: '',
+}
+
+const config = {
+  name: 'SECONDARY_APP',
+}
 
 export default function TabOneScreen() {
-  // console.log(Rive)
+  useEffect(() => {
+    const initFirebase = async () => {
+      await firebase.initializeApp(credentials, config)
+    }
+    try {
+      initFirebase()
+    } catch (e) {
+      console.log(e)
+    }
+  }, [])
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      {/* <Rive
+      <Rive
         url='https://cdn.rive.app/animations/vehicles.riv'
         fit={Fit.Contain}
         alignment={Alignment.Center}
         artboardName={'Truck'}
         style={{ width: 200, height: 200 }}
         autoplay={true}
-      /> */}
-      {/* <Rive resourceName='avatars' /> */}
-      {/* <Rive
-        fit={Fit.Contain}
-        alignment={Alignment.Center}
-        autoplay={true}
-        animationName='idlePreview'
-        resourceName={'avatars'}
-      /> */}
+      />
       <View
         style={styles.separator}
         lightColor='#eee'
