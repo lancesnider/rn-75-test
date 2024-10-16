cecfbb7c700cf4491665f15a1c483d3c6db99b21
Lance <lance@lancesnider.com>
2024-10-16 13:11:47 -0700

    Get firebase core working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,41 @@
-import { StyleSheet } from 'react-native';
+import { useRef } from 'react'
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet } from 'react-native'
+
+import EditScreenInfo from '@/components/EditScreenInfo'
+import { Text, View } from '@/components/Themed'
+// import Rive, { Alignment, Fit, RiveRef } from 'rive-react-native'
 
 export default function TabOneScreen() {
+  // console.log(Rive)
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <Rive
+        url='https://cdn.rive.app/animations/vehicles.riv'
+        fit={Fit.Contain}
+        alignment={Alignment.Center}
+        artboardName={'Truck'}
+        style={{ width: 200, height: 200 }}
+        autoplay={true}
+      /> */}
+      {/* <Rive resourceName='avatars' /> */}
+      {/* <Rive
+        fit={Fit.Contain}
+        alignment={Alignment.Center}
+        autoplay={true}
+        animationName='idlePreview'
+        resourceName={'avatars'}
+      /> */}
+      <View
+        style={styles.separator}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'
+      />
+      <EditScreenInfo path='app/(tabs)/index.tsx' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -28,4 +53,4 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-});
+})
